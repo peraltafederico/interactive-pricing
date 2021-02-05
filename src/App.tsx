bfd18d75ef0b8b5d92f596161f5531813276a5d5
Federico Peralta <fperalta@litebox.ai>
2021-02-04 23:30:29 -0300

    finish style

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,34 @@
 import React from 'react'
 import styles from './App.module.scss'
 import PricingCard from './components/PricingCard/PricingCard'
+import useWindowSize from './hooks/useWindowSize'
 
 const App = () => {
+  const { width } = useWindowSize()
+
+  const isMediumDown = width && width <= 768
+
   return (
     <div className={styles.app}>
+      <div className={styles['app__title-container']}>
+        {isMediumDown ? (
+          <h5 className={styles.app__title}>Simple, traffic-based pricing</h5>
+        ) : (
+          <h1 className={styles.app__title}>Simple, traffic-based pricing</h1>
+        )}
+        {isMediumDown ? (
+          <>
+            <p className={styles.app__description}>
+              Sign-up for our 30-day trial.
+            </p>
+            <p className={styles.app__description}>No credit card required.</p>
+          </>
+        ) : (
+          <p className={styles.app__description}>
+            Sign-up for our 30-day trial. No credit card required.
+          </p>
+        )}
+      </div>
       <div className={styles.app__card}>
         <PricingCard
           benefits={[
